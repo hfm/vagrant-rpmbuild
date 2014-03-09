d573c25982eef187e62d833b7ecabb450bb7a730
Takahiro OKUMURA <hfm.garden@gmail.com>
2014-03-09 17:44:32 +0900

    create SRPM repo to yum.repos.d

--- spec/default/rpmbuild_spec.rb
+++ spec/default/rpmbuild_spec.rb
@@ -4,8 +4,13 @@ describe package('rpm-build') do
   it { should be_installed }
 end
 
-describe file('/home/vagrant/rpmbuild/SOURCES') do
-  it { should be_directory }
-  it { should be_owned_by 'vagrant'}
-  it { should be_grouped_into 'vagrant'}
+%w(
+  /home/vagrant/rpmbuild
+  /home/vagrant/rpmbuild/SOURCES
+).each do |f|
+  describe file(f) do
+    it { should be_directory }
+    it { should be_owned_by 'vagrant'}
+    it { should be_grouped_into 'vagrant'}
+  end
 end
